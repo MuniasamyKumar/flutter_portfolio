34578f93671f11b8085378957e801ae3d3ad16ea
MuniasamyKumar <muniasamy1909@gmail.com>
2024-08-24 12:33:09 +0530

    Fix

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,9 @@ class MyApp extends StatelessWidget {
           textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
               .apply(bodyColor: Colors.white)
               .copyWith(
-                  bodyLarge: const TextStyle(color: bodyTextColor),
+                  bodyLarge: const TextStyle(
+                    color: bodyTextColor,
+                  ),
                   bodyMedium: const TextStyle(color: bodyTextColor))),
       home: const HomeScreen(),
     );
